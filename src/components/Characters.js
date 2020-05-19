dddbe6f5b2b090594f025abea811c5b0600d0460
Leamsi Escribano <leamsi.esc@gmail.com>
2020-05-19 13:26:59 -0400

    Merged character card component with characters.js, added more characters

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,8 +1,35 @@
 import React from 'react';
-import CharacterCard from './CharacterCard';
 import Footer from './Footer';
 import character_data from '../data/characters.json';
 
+const IMAGES = require.context('../images/characters');
+
+function CharacterCard(props) {
+  return (
+    <div className = "character-container">
+      <div className = "character-stats">
+        <span className = "health">{props.health}</span>
+        <span className = "hunger">{props.hunger}</span>
+        <span className = "sanity">{props.sanity}</span>
+      </div>
+      <img src = {IMAGES(`./${props.name}.png`)} alt = {`${props.name}`}/>
+      <div className = "perk-description">
+        <ul>
+        {
+          props.perks.map((perk, index) => {
+            return (
+              <li key = {index}>{perk}</li>
+            )
+          })
+        }
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+
+// Character page, it loads the character json data and creates a card for each of them.
 function Characters() {
   return (
     <div id = "character-wrapper">
